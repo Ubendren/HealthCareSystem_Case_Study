ced7a2cd60bac2dca254fac7fc87c6c89f262a8f
605512 <605512@PC324284.cts.com>
2017-06-29 16:07:09 +0530

    Jasleen--Commit

--- src/main/java/Constants/Constant.java
+++ src/main/java/Constants/Constant.java
@@ -5,5 +5,4 @@ public class Constant {
 	public String loginvalidationquery = "SELECT m.Password FROM memberlogin m WHERE m.UserId = :userid";
 	public String fetchingfailedmemberdata = "FROM FailedMemberData";
 	public String fetchingmemberdata = "FROM MemberData";
-	
 }
